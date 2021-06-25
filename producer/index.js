bc5d1748afde421fcb1f8b52f238b273158f060f
ntwari <ntwariegide2@gmail.com>
2021-06-25 22:35:14 +0200

    configured producer

--- producer/index.js
+++ producer/index.js
@@ -7,14 +7,17 @@ Producer = kafka.Producer,
 KeyedMessage = kafka.KeyedMessage
 client = new kafka.KafkaClient(),
 producer = new Producer(client),
-km = new KeyedMessage('key','message'),
-payloads = [
-    {topic : KAFKA_TOPIC,message: 'Hello kafka',partition: 0}
-]
 
-producer.on('ready', () => {
-    producer.send(payloads,(err,data) => {
-        console.log("Message recieved: ",data);
+setInterval(()=> {
+    km = new KeyedMessage('key','message'),
+    payloads = [
+        {topic : KAFKA_TOPIC,message: 'Hello kafka',partition: 0}
+    ]
+
+    producer.on('ready', () => {
+        producer.send(payloads,(err,data) => {
+            console.log("Message recieved: ",data);
+        })
     })
-})
+},3000)
 

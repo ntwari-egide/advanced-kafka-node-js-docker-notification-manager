62f1b52a28c8915c2848ce5a867b736a6a1b4614
ntwari <ntwariegide2@gmail.com>
2021-06-25 22:26:18 +0200

    added producer implementation

--- producer/index.js
+++ producer/index.js
@@ -1,1 +1,22 @@
 console.log("producer is running .....");
+
+import kafka, { KeyedMessage, Producer } from "kafka-node"
+
+const KAFKA_TOPIC = "yombi-topic-test"
+
+Producer = kafka.Producer,
+KeyedMessage = kafka.KeyedMessage
+client = new kafka.KafkaClient(),
+producer = new Producer(client),
+km = new KeyedMessage('key','message'),
+payloads = [
+    {topic : KAFKA_TOPIC,message: 'Hello kafka',partition: 0}
+]
+
+producer.on('ready', () => {
+    producer.send(payloads,(err,data) => {
+        console.log("Message recieved: ",data);
+    })
+})
+
+producer.on('error', function (err) {})

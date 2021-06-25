cd413951ad693809c80b02bf06195c3b4d7471ea
ntwari <ntwariegide2@gmail.com>
2021-06-25 23:49:59 +0200

    enabled remove all topics

---
+++ admin/index.js
@@ -0,0 +1,40 @@
+console.log("Admin is running .........");
+var kafka = require('kafka-node')
+
+const client = new kafka.KafkaClient();
+const admin = new kafka.Admin(client); 
+admin.listGroups((err, res) => {
+  console.log('consumerGroups', res);
+});
+
+createTopic("new topic createds")
+
+listAllTopics()
+
+deleteTopic("new topic createds")
+
+function listAllTopics(){
+    admin.listTopics((err, res) => {
+        console.log('topics', res);
+    });
+}
+
+
+function createTopic(topicName){
+    var topics = [{
+        topic: topicName,
+        partitions: 1,
+        replicationFactor: 2
+      }];
+    admin.createTopics(topics, (err, res) => {
+        // result is an array of any errors if a given topic could not be created
+        console.log("created topic: ",res);
+    })
+}
+
+
+function deleteTopic(topicName){
+    consumer.removeTopics([topicName], (err, removed) => {
+        console.log("deleted topic: ",removed);
+    });
+}

49f010e8d53be28e68de389796a5e3866647f590
ntwari <ntwariegide2@gmail.com>
2021-06-25 22:46:05 +0200

    resolving consumer

--- consumer/index.js
+++ consumer/index.js
@@ -1,1 +1,20 @@
 console.log("consumer is running ....");
+
+KAFKA_TOPIC = "yombi-topic-test"
+
+var kafka = require('kafka-node'),
+    Consumer = kafka.Consumer,
+    client = new kafka.KafkaClient(),
+    consumer = new Consumer(
+        client,
+        [
+            { topic: KAFKA_TOPIC, partition: 0 }
+        ],
+        {
+            autoCommit: false
+        }
+    );
+
+    consumer.on('message', (message) => {
+        console.log("Message received: ", message.value);
+    })
